161c219d438657ffa054dc86640562e5667cefa0
billy <bil@debian>
2016-09-22 17:50:45 -0500

    cleaned it up a bit

--- job_scrape.py
+++ job_scrape.py
@@ -1,17 +1,16 @@
 # scrapes city of chicagos food bev and hosp jobs page for lastest 100 postings
-
 # implement radius, zip code as cmd line args
 
 import urllib
-import time
+import datetime
 from lxml.html import parse
 import sys
 
 class craigsCrawler():
 
   def __init__(self):
-    self.url = 'https://chicago.craigslist.org/search/chc/fbh?sort=date' #food bev hosp city recent
-    self. blurb_limit = 50 #amt of txt 2 dsply 2 cnsl
+    self.url = 'https://chicago.craigslist.org/search/chc/fbh?sort=date'
+    self. blurb_limit = 50 # amount of text to print to console
 
 
 def get_page(url):
@@ -30,23 +29,22 @@ def get_page(url):
     print 'format error'
     return None
 
-def main(): # scrapes a page, parses for some shit, prints it out
+def main():
   c = craigsCrawler()
-  html = get_page(c.url) # try to get the page
+  html = get_page(c.url)
   if html != None:
-    pls = html.find_class('pl') # innermost classname for a post listing
-    for pl in pls: # so we have to iterate
-      for thing in pl: # two levels down
+    pls = html.find_class('pl')
+    for pl in pls:
+      for thing in pl:
         if thing.tag == 'time': # looking for tag -time- with attr -datetime- 
           time_posted = thing.get('datetime') # use get which takes attr and returns the value
         if thing.tag == 'a': # also looking for the job description title text
           job_text = thing.text 
-          blurb = '' # create a string to append characters to (implement proerp way)
           if len(job_text) > c.blurb_limit:
-            for i in range(0, c.blurb_limit - 1):
-              blurb += thing.text[i]
+            blurb = job_text[:c.blurb_limit]
           else:
             blurb = job_text
       print blurb + ' posted on ' + time_posted
 
 main()
+
