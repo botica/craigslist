051349cc060f0035edcb86caf921ed49e5ac65e5
billy <bil@debian>
2016-09-23 10:28:47 -0500

    added some features and job class

--- job_scrape.py
+++ job_scrape.py
@@ -5,11 +5,12 @@ import urllib
 import time
 from lxml.html import parse
 
-class craigsCrawler():
+class craigsCrawler:
 
   def __init__(self):
     self.url = 'https://chicago.craigslist.org/search/chc/fbh?sort=date'
     self. blurb_limit = 60 # amount of text to print to console
+    self.jobs = [] # to be popuated with jobs
 
   def parse_date(self, date_string):
     split1 = date_string.split('-')
@@ -21,7 +22,27 @@ class craigsCrawler():
     time = split2[1] # 24:00
     return [day, month, year, time]
 
+  def add_job(self, job):
+     self.jobs.append(job)
 
+
+class job:
+
+  def __init__(self, text, day, month, year, time):
+    self.text = text
+    self.day = day
+    self.month = month
+    self.year = year
+    self.time = time
+
+  def get_text(self):
+    return self.text
+
+  def get_day(self):
+    return self.day
+
+
+#function to get and parse html
 def get_page(url):
   try:
     print 'getting page'
@@ -40,6 +61,7 @@ def get_page(url):
 
 def main():
   c = craigsCrawler()
+  today = time.strftime('%d') # this day
   jobs_today = 0
   html = get_page(c.url)
   if html != None:
@@ -49,14 +71,19 @@ def main():
         if thing.tag == 'time': # looking for tag -time- with attr -datetime- 
           date_posted = thing.get('datetime') # use get which takes attr and returns the value
           parsed_date = c.parse_date(date_posted) # that day
-          today = time.strftime('%d') # this day
-          if parsed_date[0] == today: # if they are the same
+          if parsed_date[0] == today: # if same as today
             jobs_today += 1 # it was posted today
-        if thing.tag == 'a': # also looking for the job description title text
+        elif thing.tag == 'a': # also looking for the job description title text
           job_text = thing.text 
           blurb = job_text[:c.blurb_limit] # crop it to the blurb limit
-      print blurb + ' posted on ' + date_posted
-  print str(jobs_today) + ' jobs posted today'
+      j = job(job_text, parsed_date[0], parsed_date[1], parsed_date[2], parsed_date[3],)
+      c.add_job(j)
+      #print blurb + ' posted on ' + date_posted
+  print str(jobs_today) + ' new jobs posted today on fbh:'
+  #print str(len(c.jobs)) + ' jobs scraped total'
+  for j in c.jobs:
+    if j.get_day() == today:
+      print j.get_text()[:c.blurb_limit]
 
 main()
 
