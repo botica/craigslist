b2382fe8ec0e4e5c2bcdbe1ffbeba8fea4ae223a
billy <bil@debian>
2016-09-22 23:25:01 -0500

    comments

--- job_scrape.py
+++ job_scrape.py
@@ -1,16 +1,15 @@
-# scrapes city of chicagos food bev and hosp jobs page for lastest 100 postings
-# implement radius, zip code, as cmd args
+# scrapes craigslist's city of chicagos food bev and hosp jobs page for lastest 100 postings
+# implement radius, zip code, and as cmd args
 
 import urllib
 import time
 from lxml.html import parse
-import sys
 
 class craigsCrawler():
 
   def __init__(self):
     self.url = 'https://chicago.craigslist.org/search/chc/fbh?sort=date'
-    self. blurb_limit = 50 # amount of text to print to console
+    self. blurb_limit = 60 # amount of text to print to console
 
   def parse_date(self, date_string):
     split1 = date_string.split('-')
@@ -44,21 +43,18 @@ def main():
   jobs_today = 0
   html = get_page(c.url)
   if html != None:
-    pls = html.find_class('pl')
+    pls = html.find_class('pl') # inner most class row row posting with class name
     for pl in pls:
       for thing in pl:
         if thing.tag == 'time': # looking for tag -time- with attr -datetime- 
           date_posted = thing.get('datetime') # use get which takes attr and returns the value
-          parsed_date = c.parse_date(date_posted) # then
-          today = time.strftime('%d') #now
-          if parsed_date[0] == today:
-            jobs_today += 1
+          parsed_date = c.parse_date(date_posted) # that day
+          today = time.strftime('%d') # this day
+          if parsed_date[0] == today: # if they are the same
+            jobs_today += 1 # it was posted today
         if thing.tag == 'a': # also looking for the job description title text
           job_text = thing.text 
-          if len(job_text) > c.blurb_limit:
-            blurb = job_text[:c.blurb_limit]
-          else:
-            blurb = job_text
+          blurb = job_text[:c.blurb_limit] # crop it to the blurb limit
       print blurb + ' posted on ' + date_posted
   print str(jobs_today) + ' jobs posted today'
 
